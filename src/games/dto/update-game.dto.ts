294619dc90171842fe180922cc49b99f8117ddc1
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-05-04 16:09:16 -0600

    Add in update game details service

--- src/games/dto/update-game.dto.ts
+++ src/games/dto/update-game.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateGameDto } from './create-game.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional } from 'class-validator';
 
-export class UpdateGameDto extends PartialType(CreateGameDto) {}
+export class UpdateGameDto {
+  @IsOptional()
+  @IsString({ message: 'Name must be a string' })
+  @ApiProperty({ default: '' })
+  name: string;
+
+  @IsOptional()
+  @IsString({ message: 'Description must be a string' })
+  @ApiProperty({ default: '' })
+  description: string;
+}
