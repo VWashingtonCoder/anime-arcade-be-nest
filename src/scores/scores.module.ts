e66f7f190c8da3a12919f823dd338611b1823451
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-05-11 13:10:56 -0600

    Set up scores module

--- src/scores/scores.module.ts
+++ src/scores/scores.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ScoresService } from './scores.service';
 import { ScoresController } from './scores.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ScoresController],
   providers: [ScoresService],
+  imports: [PrismaModule],
+  exports: [ScoresService],
 })
 export class ScoresModule {}
